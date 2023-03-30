2647a54a71ec51a57f459dec632bd05a0e707f42
Waages <aaron.j.schmidt@t-online.de>
2023-03-30 17:54:03 +0200

    WIP

--- pygametest.py
+++ pygametest.py
@@ -23,10 +23,33 @@ planedetec_raw = 0		# Anzahl an detektierter Flugzeuge
 planedetec_loc = 0		# Anzahl an detek. Flug. mit Koordinaten
 lat = 48.55			# Eigene Position Breitengrad
 lon = 8.84			# Eigene Position Längengrad
+course = 0			# Eigener Kurs / Winkel zum Nordpol
 deadtime1 = 10			# Zeit seit dem letzten Signal, ab wann markiert wird
 deadtime2 = 30			# ... , ab wann nicht mehr anzeigen (Dump1090 intern ab 300s)
-scale = 0.05			# Maßstab (km pro Pixel)
+scale = 0.5			# Maßstab (km pro Pixel)
 
+
+# Bildschirmmaße definieren
+screen_width = 800
+screen_height = 410
+screen = (screen_width, screen_height)
+Menuebreite = 100
+Menuekante = int(screen_width - Menuebreite)
+# Eigene Positon im Bildschirm
+centerposx = (screen_width - Menuebreite) / 2 + 0
+centerposy = screen_height / 2 + 0
+centerpos = (int(centerposx), int(centerposy))
+
+
+# Farben definieren
+white = (255, 255, 255)
+gray = (128, 128 ,128)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 128, 0) 
+
+
+###--FUNKTIONEN--------------------
 
 # GPS initialisieren
 gps = Serial('/dev/ttyS0', 9600)
@@ -34,6 +57,7 @@ gps = Serial('/dev/ttyS0', 9600)
 def read_gps_data():
 	global lat
 	global lon
+	global gps_signal
 	global gps_thread_running
 	while gps_thread_running:	
 		line = gps.readline().decode('utf-8')
@@ -43,8 +67,9 @@ def read_gps_data():
 				gps_signal = True
 				lat = float(data[2][:2]) + float(data[2][2:]) / 60
 				lon = float(data[4][:3]) + float(data[4][3:]) / 60
+#				print("GPS")
 			else:
-				print("Kein GPS-Signal")
+#				print("Kein GPS-Signal")
 				gps_signal = False
 		time.sleep(0.1)
 
@@ -76,47 +101,51 @@ def Winkel(lat, lon, planelat, planelon):
 	alpha = math.degrees(theta)
 	return alpha
 
-# Bildschirmmaße definieren
-screen_width = 800
-screen_height = 400
-screen = (screen_width, screen_height)
-# Eigene Positon im Bildschirm
-centerposx = screen_width / 2 + 0
-centerposy = screen_height / 2 + 0
-centerpos = (centerposx, centerposy)
+# Pixelkoordinaten bestimmen
+def getPixelx(planedist, planeangl):
+	angl = planeangl - course
+	dist = planedist / scale
+	x = int(round(math.sin(math.radians(angl)) * dist))
+#	print("AbsolutX:", abs(x))
+	if x > (screen_width / 2):
+		x = (screen_width / 2)
+	if x < (screen_width / -2):
+		x = (screen_width / -2)
+	return x
+
+def getPixely(planedist, planeangl):
+	angl = planeangl - course
+	dist = planedist / scale
+	y = int(round(math.cos(math.radians(angl)) * dist))
+#	print("AbsolutY:", abs(y))
+	if y > (screen_height / 2):
+		y = (screen_height / 2)
+	if y < (screen_height / -2):
+		y = (screen_height / -2)
+	return y
 
 # Fenster erstellen und Fenstertitel vergeben
 screen = pygame.display.set_mode(screen , pygame.RESIZABLE)
 pygame.display.set_caption('Hier steht Text')
 
-# Farben definieren
-white = (255, 255, 255)
-gray = (128, 128 ,128)
-black = (0, 0, 0)
-
 # Font definieren
-font = pygame.font.Font(None, 20)
-
-# Kreis Eigenschaften
-circle_radius = 100
-circle_x = screen_width // 2
-circle_y = screen_height // 2
+font = pygame.font.Font(None, 27)
+fontS = pygame.font.Font(None, 20)
 
 # Quit-Button Eigenschaften
-button_width = 120
+button_width = Menuebreite
 button_height = 60
 button_x = screen_width - button_width
 button_y = screen_height - button_height
 
-time.sleep(0.5)
-
+#time.sleep(1.5)
 
 pygame.display.update()
-print("123")
+#print("123")
 
-print("Testabstand: ",Abstand(lat,lon,(lat-1),(lon-1)))
-print("Testwinkel: ",Winkel(lat,lon,(lat-1),(lon-1)))
-
+#print("Testabstand: ",Abstand(lat,lon,(lat-1),(lon-1)))
+#print("Testwinkel: ",Winkel(lat,lon,(lat-1),(lon-1)))
+#print("X: ", getPixelx(20, 45), "Y: ",getPixely(20,45))
 
 running = True
 while running:
@@ -132,17 +161,19 @@ while running:
 	# Hintergrund einfärben
 	screen.fill(white)
 	
+	pygame.draw.line(screen, gray, [Menuekante - 4, 0], [Menuekante - 4, screen_height], 2)
+
 	# Variable anzeigen
-	lattext = font.render(str(lat), True, black)
-	lontext = font.render(str(lon), True, black)
-	screen.blit(lattext, (19, 10))
-	screen.blit(lontext, (19, 30))
+	lattext = font.render(str(round(lat, 5)), True, black)
+	lontext = font.render(str(round(lon, 5)), True, black)
+	screen.blit(lattext, (Menuekante, 40))
+	screen.blit(lontext, (Menuekante, 60))
 
 	# GPS-Status anzeigen
-	if gps_signal == True:
-		screen.blit(font.render("GPS-Empfang", True, black), (19, 50))
+	if gps_signal:
+		screen.blit(font.render("GPS", True, green), (Menuekante, 10))
 	else:
-		screen.blit(font.render("Kein GPS", True, black), (19, 50))
+		screen.blit(font.render("Kein GPS", True, red), (Menuekante, 10))
 
 	#Schaltfläche
 	pygame.draw.rect(screen, gray, (button_x, button_y, button_width, button_height))
@@ -150,6 +181,8 @@ while running:
 	text_rect = text.get_rect(center=(button_x + button_width // 2, button_y + button_height // 2))
 	screen.blit(text, text_rect)
 
+	# Eigene Position darstellen
+	pygame.draw.circle(screen, green, (centerpos) , 10)
 
 	# Dump1090 auslesen
 	response = urllib.request.urlopen(url)
@@ -165,7 +198,24 @@ while running:
 				planelon = aircraft.get('lon')
 				planedist = Abstand(lat, lon, planelat, planelon)
 				planeangl = Winkel(lat, lon, planelat, planelon)
-				print("Abstand: ",planedist, "Winkel: ", planeangl)
+#				print("Abstand: ",planedist, "Winkel: ", planeangl)
+				planepixelpos = (int(centerposx + getPixelx(planedist, planeangl)), int(centerposy - getPixely(planedist, planeangl)))
+				if aircraft.get('seen') > deadtime1:
+					planecolor = gray
+				else:
+					planecolor = black
+				pygame.draw.circle(screen, planecolor, (planepixelpos) , 10)
+				screen.blit(fontS.render(aircraft.get('hex'), True, black), tuple(map(sum, zip(planepixelpos,(10,-10)))))
+
+#	screen.blit(font.render(("Flugzeuge:", planedetec_raw), True, black, (19, (screen_height - 40))))
+#	screen.blit(font.render(("Davon mit Loc:", planedetec_loc), True, black, (19, ))))
+
+	rawtext = font.render(("R: " + str(planedetec_raw)), True, black)
+	loctext = font.render(("L: " + str(planedetec_loc)), True, black)
+	screen.blit(rawtext, (Menuekante, (screen_height - (button_height + 50))))
+	screen.blit(loctext, (Menuekante, (screen_height - (button_height + 30))))
+
+
 #	print("Anzahl: ",planedetec_raw," Davon Loc: ", planedetec_loc)
 #	time.sleep(1)
 	pygame.display.update()
@@ -176,4 +226,3 @@ gps_thread_running = False
 gps_thread.join()
 pygame.quit()
 sys.exit()
-
